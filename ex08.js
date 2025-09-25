e3cb847c413cdb31ebe22654aebce3557d363a8f
Suzuna Arakaki <suzuna.arakaki@gmail.com>
2025-09-24 20:58:26 -0700

    exercise 08 is done

--- ex08.js
+++ ex08.js
@@ -1,3 +1,7 @@
 // Join Exercise
-// Instructions: Create an array of at least five different words as strings. Write code to join all the words into a single string, with each word separated by a space.
+// Instructions: Create an array of at least five different words as strings. 
+// Write code to join all the words into a single string, with each word separated by a space.
 
+const words = ["Hi", "Hey", "Yup", "Lol", "Same"];
+
+console.log(words.join(" "));
